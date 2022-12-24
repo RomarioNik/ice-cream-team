0a74ab0b2e81259de4f5a3874d6f8ce5d5d3d687
Roman <rprikhoda@ukr.net>
2022-12-25 01:09:29 +0200

    some changes

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,11 +1,10 @@
 new Swiper('.mySwiper', {
-    slidesPerView: 1,
-    centeredSlides: true,
-    spaceBetween: 100,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 100,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
